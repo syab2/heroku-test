81ccce4179f0d5722488a731e2b2e3810c9285d3
syab2 <kolakrylov6@gmail.com>
2022-04-03 11:58:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return """<h1>Hello world!</h1><br>
+                        <img src='static/kot.jpg'>"""
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
