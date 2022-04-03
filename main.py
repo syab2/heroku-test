c9ffbe940b74844f40562babde62c905874786ac
syab2 <kolakrylov6@gmail.com>
2022-04-03 12:32:42 +0300

    mamamia

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return """<h1>Hello world!</h1><br>
-                        <img src='static/kot.jpg'>"""
+                        <img src='static/mamamia.jpg'>"""
 
 
 if __name__ == '__main__':
